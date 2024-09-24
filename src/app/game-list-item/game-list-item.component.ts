288f29d65a9f6dee9157f785444f58d2f6e0bb54
francis biller <w0855164@myscc.ca>
2024-09-24 10:43:31 -0400

    Added version 2.0 and GameList and GameListItem

---
+++ src/app/game-list-item/game-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './game-list-item.component.html',
+  styleUrl: './game-list-item.component.css'
+})
+export class GameListItemComponent {
+
+}
