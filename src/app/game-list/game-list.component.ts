94249758203b11f91185a733e98e1d2d172a1c19
francis biller <w0855164@myscc.ca>
2024-10-01 10:44:47 -0400

    assigment 4 , created service and named it

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -17,30 +17,4 @@ import {NgClass, NgForOf} from "@angular/common";
   styleUrl: './game-list.component.css'
 })
 export class GameListComponent {
-  contentItems: Games[] = [
-    {
-      title: 'Call Of duty Modern Warfare 3',
-      genre: 'action',
-      yearReleased: 2023,
-      productionCompany: 'Infinity ward'
-    },
-    {
-      title: 'Fortnite',
-      genre: 'action',
-      yearReleased: 2017,
-      productionCompany: 'Epic games'
-    },
-    {
-      title: 'Minecraft',
-      genre: '3D sandbox',
-      yearReleased: 2011,
-      productionCompany: 'Mojang studios'
-    },
-    {
-      title: 'roblox',
-      genre: 'First Person Game',
-      yearReleased: 2006,
-      productionCompany: 'Roblox corporation'
-    },
-  ];
 }
