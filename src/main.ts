fc7ab3c87dd4908941c97a92e88412aad3363e7c
francis biller <w0855164@myscc.ca>
2024-11-18 22:26:46 -0500

    finished

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,31 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
 
-import {provideRouter, Routes } from "@angular/router";
-import {GameListComponent} from "./app/game-list/game-list.component";
-import {GameListItemComponent} from "./app/game-list-item/game-list-item.component";
-import {ModifyGameItemComponent} from "./app/modify-game-item/modify-game-item.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
-
-
-
-
-
- const routes: Routes = [
-   {path: '',redirectTo: 'shared/games', pathMatch: 'full'},
-{ path: 'shared/games', component: GameListComponent},
-{ path: 'shared/games/:id', component: GameListItemComponent},
-   {path: 'modify-game', component: ModifyGameItemComponent},
-   {path: '**', component: PageNotFoundComponent}
+const routes: Routes = [
+  { path: '', redirectTo: 'games', pathMatch: 'full' },
+  {
+    path: 'games',
+    loadComponent: () =>
+      import('./app/game-list/game-list.component').then(m => m.GameListComponent)
+  },
+  {
+    path: 'games/:id',
+    loadComponent: () =>
+      import('./app/game-list-item/game-list-item.component').then(m => m.GameListItemComponent)
+  },
+  {
+    path: 'modify-game',
+    loadComponent: () =>
+      import('./app/modify-game-item/modify-game-item.component').then(m => m.ModifyGameItemComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./app/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)
+  }
 ];
 
-
 bootstrapApplication(AppComponent, {
-  providers:[provideRouter(routes)],
-});
+  providers: [provideRouter(routes)],
+}).then(() => console.log('Application started successfully'));
