e0a332f9b219b6e3d8a9cc463449c3c860d35d24
francis biller <w0855164@myscc.ca>
2024-11-22 21:30:09 -0500

    finish havee to change something

---
+++ src/app/directives/highlight-on-focus.directive.ts
@@ -0,0 +1,25 @@
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
+
+@Directive({
+  selector: '[appHighlightOnFocus]',
+  standalone: true
+})
+export class HighlightOnFocusDirective {
+
+  @Input() appHighlightOnFocus = '';
+
+  constructor(private el: ElementRef) {
+  }
+
+  @HostListener('focus') onfocus() {
+    this.focus(this.appHighlightOnFocus || 'yellow');
+  }
+
+  @HostListener('blur') blur() {
+    this.focus('');
+  }
+
+  private focus(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
